c692da27c579803dcad2bd3a807ef44d551bed8c
BabrIrk <neurozx@gmail.com>
2020-09-26 21:00:42 +0800

    как тут это все работает?

---
+++ src/main/java/com/hackathon/rosatom/atombabr/AtombabrApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.rosatom.atombabr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtombabrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtombabrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/rosatom/atombabr/AtombabrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.rosatom.atombabr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtombabrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
